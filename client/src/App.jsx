d29c91d94ef36d053d7e416b1793b1362c29290b
Devanshi-4831 <bhungaliyadevanshi318@gmail.com>
2025-07-30 13:41:00 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -18,7 +18,7 @@ const App = () => {
     const handleGetUser = async () => {
       try {
         const { data } = await axios.get(
-          "http://localhost:4000/api/v1/user/me",
+          "https://mern-stack-task-manager-1-1qos.onrender.com/api/v1/user/me",
           { withCredentials: true }
         );
         setIsAuthenticated(true);
